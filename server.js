abd0fa7bf876c8bfe0f9d4c9a28b2ac9add06fb9
Bitmona <alexbrown422@gmail.com>
2018-09-23 07:50:01 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
-const Discord = require("discord.js");
-const bot = new Discord.Client();
-const config = require("./config.json");
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
-bot.on('ready', () => {
-  console.log(`Logged in as ${bot.user.username}!`);
-   bot.user.setActivity('Made By BitJSDevs');
+client.on('ready', () => {
+    console.log('I am ready!');
 });
-bot.on("message", (message) => { 
- // if (!message.content.startsWith(config.prefix) || message.author.bot) return;
-  if (message.content.startsWith(config.prefix + "author")) {
-    message.reply('BitJSDevs');
-    
-    bot.login(process.env.BOT_TOKEN);
+
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
+});
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
