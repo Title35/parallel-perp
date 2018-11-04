75d9456c1377b9917f4ce466a9e5b03bf1982417
Bitmona <alexbrown422@gmail.com>
2018-11-04 10:24:14 -0500

    Activity

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const client = new Discord.Client();
 client.on('ready', () => {
     console.log('I am ready!');
 });
+client.user.setActivity("Watching the Hangout™!"); 
 
 client.on('message', message => {
     if (message.content === '`ping') {
