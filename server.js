3109c57d2eb394a6fb04815d89eaffa40dddb2c8
Bitmona <alexbrown422@gmail.com>
2018-11-04 14:21:47 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const client = new Discord.Client();
 client.on('ready', () => {
     console.log('I am ready!');
 });
+let cont = message.content.slice(prefix.length).split(“ “)
+let args = cont.slice(1);
 //client.user.setActivity("The Hangout™", {type: "WATCHING"}); 
 
 client.on('message', message => {
@@ -89,6 +91,35 @@ client.on('message', message => {
     message.channel.fetchMessages({ limit: messagecount })
   .then(messages => message.channel.bulkDelete(messages));
     message.channel.send('Succesfully purged!')	
+    }
+});
+client.on('message', message => {
+    if (msg.startsWith('`purge')) {         
+async function purge() {
+            message.delete(); 
+            
+            if (!message.member.roles.find("name", “Staff Member”)) { 
+                message.channel.send('You need the \`Staff Member\` role to use this command.'); 
+            }
+
+            
+            if (isNaN(args[0])) {
+               
+                message.channel.send('Please use a number as your arguments. \n Usage: ' + prefix + 'purge <amount>');               
+  return;
+            }
+
+            const fetched = await message.channel.fetchMessages({limit: args[0]}); 
+            console.log(fetched.size + ' messages found, deleting...'); 
+           
+            message.channel.bulkDelete(fetched)
+                .catch(error => message.channel.send(`Error: ${error}`)); // If it finds an error, it posts it into the channel.
+
+        }
+
+       
+        purge(); 
+
     }
 });
 
