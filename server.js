d2bdadf9ef5bc7c499048b55ab433555fd4b84b4
Bitmona <alexbrown422@gmail.com>
2018-11-04 08:56:39 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
+    if (message.content === prefix + 'ping') {
     	message.reply('pong');
   	}
 });
