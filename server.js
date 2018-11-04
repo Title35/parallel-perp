a3dd307a7fb0969d1080fb529a997bde2267d363
Bitmona <alexbrown422@gmail.com>
2018-11-04 10:12:14 -0500

    Update server.js

--- server.js
+++ server.js
@@ -21,10 +21,12 @@ client.on('message', message=> {
 client.on('message', message => {
     if (message.content === '`help') {
     	message.channel.send("You've got mail! :mailbox_with_mail:")
-  	message.author.send({{"embed":{"image":{"url":"https://cdn.discordapp.com/attachments/496703299672735756/508658340625383444/Hangout2.jpg"},"footer":{"text":"The Hangout™"},"color":3553598,"fields":[{"name":"Supreme Leader","value":"YourUniversal_Salad","inline":false},{"name":"Hangout Overseer","value":"UnderPerfection","inline":false},{"name":"President","value":"YourGerman_Salad","inline":false},{"name":"Vice President","value":"Bitmona","inline":false}]}}
- 
+  	message.author.send({embed: {
+  color: 3447003,
+  description: "A very simple Embed!"
+
     }
-});}});
+});}}); 
 client.on('message', message => {
     if (message.content === '`Ping') {
     	message.channel.send('Pong')
@@ -42,8 +44,16 @@ client.on('message', message => {
 });
 client.on('message', message => {
     if (message.content === '`Leaders') {
-    	
-  	}
+    		message.author.send({{"embed":{
+          "image":{"url":"https://cdn.discordapp.com/attachments/496703299672735756/508658340625383444/Hangout2.jpg"},
+        "footer":{"text":"The Hangout™"},
+        "color":3553598,
+        "fields":[{"name":"Supreme Leader","value":"YourUniversal_Salad","inline":false},{"name":"Hangout Overseer","value":"UnderPerfection","inline":false},{"name":"President","value":"YourGerman_Salad","inline":false},{"name":"Vice President","value":"Bitmona","inline":false
+           }]}}
+ 
+    }
+});
+  	
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
