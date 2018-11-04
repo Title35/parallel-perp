6eceaa76f1eb2e42bce3c8003d623881ea9d7d02
Bitmona <alexbrown422@gmail.com>
2018-11-04 09:43:38 -0500

    Update server.js

--- server.js
+++ server.js
@@ -19,13 +19,28 @@ client.on('message', message => {
     if (message.content === '`help') {
     	message.channel.send("You've got mail! :mailbox_with_mail:")
   	message.author.send({embed: {
-  color: 3447003,
-  description: "A very simple Embed!"
+  _embed #channel title=Leaders | description=The leaders of The Hangout™ | thumbnail=https://cdn.discordapp.com/attachments/496703299672735756/508652160939065344/Hangout2.jpg | field=name=Supreme Leader - value=YourUniversal_Salad | field=name=Hangout Overseer value=UnderPerfection inline=no | field=name=President value=YourGerman_Salad inline=no | field=name=Vice President value=Bitmona inline=no | color=#7e77ff
     }
 });}});
-
-
-
+client.on('message', message => {
+    if (message.content === '`Ping') {
+    	message.channel.send('Pong')
+  	}
+});
+client.on('message', message => {
+    if (message.content === '`Author') {
+    	message.channel.send(":fire: BitJSDevs :fire:")
+  	}
+});
+client.on('message', message => {
+    if (message.content === '`author') {
+    	message.channel.send(":fire: BitJSDevs :fire:")
+  	}
+});
+client.on('message', message => {
+    if (message.content === '`Leaders') {
+    	
+  	}
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
