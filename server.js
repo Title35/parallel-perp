c95f244aa5e461765a5e8b7347615d3e12bacd14
Bitmona <alexbrown422@gmail.com>
2018-11-04 19:14:55 -0500

    Fix

--- server.js
+++ server.js
@@ -4,56 +4,55 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
+
 client.on('message', message => {
-    let role = message.guild.roles.find(r => r.name === "Hangout Members");
-let member = message.author();
     if (message.content === '=ping') {
     	message.channel.send('Pong')
   	}
-
-
+});
+client.on('message', message=> {
     if (message.isMentioned(client.user)) {
     message.channel.send('I am Awake!');
 
         
 }
+});
 
-
-
+client.on('message', message => {
     if (message.content === '=help') {
     	message.channel.send("You've got mail! :mailbox_with_mail:")
   	message.author.send({embed: {
   color: 3447003,
   description: "Help coming soon!"
 
-}});
     }
- 
+});}}); 
 
-
+client.on('message', message => {
     if (message.content === '=Help') {
     	message.channel.send("You've got mail! :mailbox_with_mail:")
   	message.author.send({embed: {
   color: 3447003,
   description: "Help coming soon!"
 
-}});}
- 
-
+    }
+});}}); 
+client.on('message', message => {
     if (message.content === '=Ping') {
     	message.channel.send('Pong')
   	}
-
-
+});
+client.on('message', message => {
     if (message.content === '=Author') {
     	message.channel.send(":fire: BitJSDevs :fire:")
   	}
-
-
+});
+client.on('message', message => {
     if (message.content === '=author') {
     	message.channel.send(":fire: BitJSDevs :fire:")
   	}
-
+});
+client.on('message', message => {
     if (message.content === '=coinflip') {
 function doRandHT() {
 var rand = ['HEADS!','TAILS!'];
@@ -69,44 +68,46 @@ return rand[Math.floor(Math.random()*rand.length)];
 message.channel.send({ embed });
 
     }
+});
 
-
-
+client.on('message', message => {
     if (message.content === '=creator') {
     	message.channel.send('**Server Creator:** YourUniversal_Salad')
   	}
+});
 
-
-
+client.on('message', message => {
     if (message.content === '=Creator') {
     	message.channel.send('Server Creator: YourUniversal_Salad')
   	}
-
+});
     
+    client.on('guildMemberAdd', member => {
   // Send the message to a designated channel on a server:
   const channel = member.guild.channels.find(ch => ch.name === 'welcome');
   // Do nothing if the channel wasn't found on this server
   if (!channel) return;
   // Send the message, mentioning the member
   channel.send(`Welcome ${member} to The Hangout!™ Please look at #rules-info before doing anything in the server. If you need any help, say -new in #bot-commands! Have fun!`);
+});
 
-
-
+client.on('message', message => {
     if (message.content === '=profile') {
     	message.channel.send('Your Profile:')
         client.fetchUser(message.author).then(myUser => {
     message.channel.send(myUser.avatarURL); // My user's avatar is here!
-
+});
   	}
- if (message.content === '=cookie') {
-    	message.channel.send(':cookie:')
-  	}
+});
 
-  /*  if (message.content === '=verify') {
+client.on('message', message => {
+    let role = message.guild.roles.find(r => r.name === "Hangout Members");
+let member = message.author();
+    if (message.content === '=verify') {
        member.addRole(role).catch(console.error);
 channel.fetchMessage(message.author).then(message.delete).catch(console.error);
         
-  	} */
+  	}
 });
 
 // THIS  MUST  BE  THIS  WAY
