3cb6ddbce93b79473dde9f7ad26d1576b1207ce5
Bitmona <alexbrown422@gmail.com>
2018-11-06 09:59:06 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,14 @@ client.on('ready', () => {
     client.user.setPresence({ game: { name: "on The Hangout™ Server!" }, status: 'dnd' })
     .catch(console.error);
    const channel = client.channels.find(ch => ch.name === 'bot-status');
-    channel.send("I am ready to go to work! :spongebob:")
+    //channel.send("I am ready to go to work! :spongebob:")
+    channel.send({embed: {
+  color: 3447003,
+  description: "I'm ready, I'm ready! To go to work!"
+  "image": {
+                "url": "https://cdn.discordapp.com/attachments/420986851005038592/509377696271171586/sticker-6-112.png",
+                }      
+}});
 });
 
 
